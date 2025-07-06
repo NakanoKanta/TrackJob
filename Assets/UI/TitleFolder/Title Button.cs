064b7f1bf0020d00f9f37221640a8fd50fb7349b
ki28ryu <ki28ryu@icloud.com>
2025-07-06 16:18:01 +0900

    [add]スタートボタンの追加

---
+++ Assets/UI/TitleFolder/Title Button.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//シーンを読み込むために必要
+using UnityEngine.SceneManagement;
+
+public class TitleButton : MonoBehaviour
+{
+    public void StartButton()
+    {
+        SceneManager.LoadScene("");
+    }
+}
