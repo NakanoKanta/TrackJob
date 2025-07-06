e9db26578870d6b80c38942eb18241f7886cc4ac
kantanvgat <kantan.vgat@gmail.com>
2025-07-06 17:36:53 +0900

    <add>InGame,OutGameフォルダの制作、Playerオブジェクトの配置

---
+++ Assets/Round/RoundManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundManager : MonoBehaviour
+{
+    [SerializeField] int _maxRoundWin = 2;
+    private int _player1Wins = 0;
+    private int _player2Wins = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
