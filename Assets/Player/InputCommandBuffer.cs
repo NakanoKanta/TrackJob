e26847d59f338c38c2f3d7472897448799948841
kaitomiyamoto1 <kaitomiyamoto1@gmail.com>
2025-07-07 19:52:45 +0900

    <add>CommandBufferの代わりに新しくInputBufferを作成

---
+++ Assets/Player/InputCommandBuffer.cs
@@ -0,0 +1,68 @@
+using JetBrains.Annotations;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputCommandBuffer
+{
+    private List<InputData> _buffer = new List<InputData>();
+    private float _timeWindow = 1.0f;
+    private int _maxSize = 8;
+
+    public void AddInput(InputType input)
+    {
+        _buffer.Add(new InputData(input, Time.time));
+        if (_buffer.Count > _maxSize)
+        {
+            _buffer.RemoveAt(0);
+        }
+        CleanOldInputs();
+    }
+
+    public bool CheckCommand(InputType[] sequence)
+    {
+        if (_buffer.Count < sequence.Length) return false;
+        int start = _buffer.Count - sequence.Length;
+        for (int i = 0; i < sequence.Length; i++)
+        {
+            if (_buffer[start + i]._type != sequence[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void Clear() => _buffer.Clear();
+
+    public void CleanOldInputs()
+    {
+        float currentTime = Time.time;
+        _buffer.RemoveAll(input => currentTime - input._time > _timeWindow);
+    }
+
+    public string GetBufferString()
+    {
+        string result = "";
+        foreach (var input in _buffer)
+        {
+            result += GetSymbol(input._type) + "";
+        }
+        return result;
+    }
+
+    private string GetSymbol(InputType type)
+    {
+        return type switch
+        {
+            InputType.Down => "«",
+            InputType.Up => "ª",
+            InputType.Left => "©",
+            InputType.Right => "¨",
+            InputType.Punch => "P",
+            InputType.Guard => "K",
+            _ => "?"
+        };
+    }
+}
