cfe0b180ac2803aad2184e43a9d678cda92b3914
kantanvgat <kantan.vgat@gmail.com>
2025-07-11 16:21:49 +0900

    <Update>AttackHitboxスクリプトにhasHitをfalseにする処理を追加

--- Assets/Round/AttackHitbox.cs
+++ Assets/Round/AttackHitbox.cs
@@ -6,6 +6,7 @@ public class AttackHitbox : MonoBehaviour
 {
     [SerializeField] int _damage = 0;
     private bool hasHit = false;
+    public float _invalidTime = 0.2f;
 
     void OnTriggerEnter2D(Collider2D other)
     {
@@ -15,7 +16,15 @@ public class AttackHitbox : MonoBehaviour
         {
             enemyHealth.TakeDamage(_damage);
             hasHit = true;
+            StartCoroutine(DisableHitboxTemporarily());
 
         }
+    }
+    private System.Collections.IEnumerator DisableHitboxTemporarily()
+    {
+        gameObject.SetActive(false);
+        yield return new WaitForSeconds(_invalidTime);
+        gameObject.SetActive(true);
+        hasHit = false;
     }
 }
